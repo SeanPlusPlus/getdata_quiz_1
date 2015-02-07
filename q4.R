97d3b856e9703c80b5247b3b7e86bfda8b888b26
Sean Stephenson <sms@pixar.com>
2015-02-07 15:57:47 -0800

    add restaurants

---
+++ q4.R
@@ -0,0 +1,26 @@
+# Question 4
+# Read the XML data on Baltimore restaurants from here:
+# https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml
+# How many restaurants have zipcode 21231?
+
+################################################################################
+getRestaurants <- function() {
+################################################################################
+  library(XML)
+  url   <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
+  xData <- getURL(url)
+  doc   <- xmlParse(xData)
+}
+
+################################################################################
+main <- function() {
+################################################################################
+  doc <- getRestaurants()
+  rootNode <- xmlRoot(doc)
+  rootNode <- rootNode[[1]]
+  xpathSApply(rootNode, "//zipcode", xmlValue)
+  zipCodes <- xpathSApply(rootNode, "//zipcode", xmlValue)
+  zipCodes <- table(zipCodes)
+  print(zipCodes[names(zipCodes) == "21231"])
+  # 127
+}
