05338f60c40b813a51c1ba7e39e0d74b1f1ef186
Sean Stephenson <sms@pixar.com>
2015-02-07 11:57:29 -0800

    stub out q3

---
+++ q3.R
@@ -0,0 +1,21 @@
+# Question 3
+# Download the Excel spreadsheet on Natural Gas Aquisition Program here:
+# https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx
+# Read rows 18-23 and columns 7-15 into R and assign the result to a variable called: dat
+# What is the value of: sum(dat$Zip*dat$Ext,na.rm=T)
+
+################################################################################
+getData <- function() {
+################################################################################
+
+
+}
+
+################################################################################
+main <- function() {
+################################################################################
+
+
+}
+
+main()
