2db81c306d2d6d1ae9ab4a81975118148144eb3f
Sean Stephenson <sms@pixar.com>
2015-02-07 16:29:57 -0800

    uncomment file fetch

--- q5.R
+++ q5.R
@@ -22,8 +22,8 @@ getHousingData <- function() {
   }
 
   # fetch and return
-  #fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv"
-  #download.file(fileUrl, destfile = "./data/community2006.csv", method = "curl")
+  fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv"
+  download.file(fileUrl, destfile = "./data/community2006.csv", method = "curl")
   data <- fread("./data/community2006.csv")
   return(data)
 }
